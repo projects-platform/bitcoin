1c60d67b648c75dedf049ef107bd166809d4b373
hvkcoder <hvkcoder@163.com>
2018-09-07 17:07:27 +0800

    Add http

--- src/main.go
+++ src/main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
-	"./core"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+
+	"./core"
 )
 
-func main() {
+var blockchain *core.Blockchain
 
-	bc := core.NewBlockchain()
+func run() {
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/write", WriteHandler)
+	http.ListenAndServe(":3000", nil)
+}
 
-	bc.AppendBlock("Send 1 bit")
-	bc.AppendBlock("Send 2 bit")
-
-	for _, value := range bc.Blocks {
-		fmt.Printf("%x  %x  %s\n",value.Hash, value.PrevBlockHash,value.Data)
+func IndexHandler(w http.ResponseWriter, req *http.Request) {
+	v, err := json.Marshal(blockchain)
+	if err != nil {
+		fmt.Printf(err.Error())
 	}
+
+	io.WriteString(w, string(v))
+}
+
+func WriteHandler(w http.ResponseWriter, req *http.Request) {
+	blockchain.AppendBlock(req.FormValue("data"))
+	io.WriteString(w, "完成新区块的写入")
+}
+
+func main() {
+	blockchain = core.NewBlockchain()
+	run()
 }
